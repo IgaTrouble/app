ad8e1cbd96bec42777686c1a1a5d9aeebc0aa44b
tadeusz <tadeusz.drabarek@wp.pl>
2017-11-29 22:56:48 +0100

    TD.21. Wykres słupkowy ze statystykami działa

--- Controller/StatystykiController.java
+++ Controller/StatystykiController.java
@@ -15,6 +15,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -26,6 +31,8 @@ public class StatystykiController {
 	public DBConnector db;
 	private ObservableList grupy;
 	private ObservableList osoby;
+    @FXML
+    private StackedBarChart<Double, String> sbc;
     @FXML
     private Button btn_show;
 
@@ -73,6 +80,51 @@ public class StatystykiController {
     	cb_grupa.setItems(grupy);
     	this.pobierz_osoby();
     	cb_osoba.setItems(osoby);
+    	
+    	this.sbc();
+    }
+    
+    @FXML
+    private BarChart<?, ?> bs;
+    
+    @FXML
+    private CategoryAxis x;
+
+    @FXML
+    private NumberAxis y;
+    
+    public void sbc() throws SQLException
+    {
+    	
+    	/*XYChart.Series series1 = new XYChart.Series<>();
+    	XYChart.Series series2 = new XYChart.Series<>();
+    	XYChart.Series series3 = new XYChart.Series<>();
+    	//series1.setName("Kursant1");
+    	series1.getData().add(new XYChart.Data("Kursant 1", 50.55));
+    	series1.getData().add(new XYChart.Data("Kursant 2", 20.55));
+    	series1.getData().add(new XYChart.Data("Kursant 3", 30.55));
+    	bs.getData().addAll(series1);
+    	*/
+    	DBConnector db = new DBConnector();
+        Connection conn = db.Connection();
+    	String sql="select concat(l.imie,' ',l.nazwisko), avg(t.wynik) from testy t inner join loginy l on l.email=t.kursant where t.wynik is not null group by concat(l.imie,' ',l.nazwisko);";
+	 	
+	 	ResultSet rs = conn.createStatement().executeQuery(sql);
+        System.out.println(sql);
+        XYChart.Series s = new XYChart.Series<>();
+        while(rs.next()){
+        	
+        	s.getData().add(new XYChart.Data(rs.getString(1), rs.getDouble(2)));    	
+        	
+        	
+        }   
+        bs.getData().addAll(s);
+	 	
+	 	
+	 	conn.close();
+    	
+    	
+    	
     }
     
     
